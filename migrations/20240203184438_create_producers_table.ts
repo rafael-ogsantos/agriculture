20a7c2a8472c7561924567e08f80ec00ddce8f40
Galvao <rafaeloliveira@febracis.com.br>
2024-02-04 13:44:11 -0300

    create table producers

--- migrations/20240203184438_create_producers_table.ts
+++ migrations/20240203184438_create_producers_table.ts
@@ -1,8 +1,7 @@
 import type { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
-    return knex.schema.createTable('farmers', function(table) {
+    return knex.schema.createTable('producers', function(table) {
         table.increments();
         table.string('cpf_or_cnpj').unique().notNullable();
         table.string('producer_name').notNullable();
@@ -12,13 +11,11 @@ export async function up(knex: Knex): Promise<void> {
         table.decimal('total_area_hectares').notNullable();
         table.decimal('arable_area_hectares').notNullable();
         table.decimal('vegetation_area_hectares').notNullable();
-        table.enu('crops', ['Soy', 'Corn', 'Cotton', 'Coffee', 'Sugarcane']).notNullable();
+        table.json('crops').notNullable(); // Changed from table.enu to table.json
         table.timestamps(true, true);
     });
 }
 
-
 export async function down(knex: Knex): Promise<void> {
-    return knex.schema.dropTable('farmers');
+    return knex.schema.dropTable('producers');
 }
-

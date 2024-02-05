af311f3cc674d129127cb3b4764128c356a86f87
Galvao <rafaeloliveira@febracis.com.br>
2024-02-05 00:02:47 -0300

    ajustment string to number

--- src/modules/producers/validations/areaValidation.ts
+++ src/modules/producers/validations/areaValidation.ts
@@ -4,7 +4,8 @@ import { ProducerInput } from "../schema";
 export class AreaValidation implements ValidationStrategy {
     validate(producer: ProducerInput) {
         const { totalArea, arableArea, vegetationArea } = producer;
-        if (totalArea < arableArea + vegetationArea) {
+        const sum = Number(arableArea) + Number(vegetationArea);
+        if (sum > totalArea) {
             throw new Error('The arable area and vegetation area cannot be greater than the total area');
         }
     }

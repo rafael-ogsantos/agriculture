9cfa58a10b6d0a76e48ff001acceb96095f40590
Galvao <rafaeloliveira@febracis.com.br>
2024-02-05 14:43:58 -0300

    config cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 
+import cors from 'cors';
 import producersRouter from './api/producers/router';
 
 const app = express();
@@ -7,11 +8,13 @@ process.on('uncaughtException', function (err) {
   console.log(err);
 });
 
+app.use(cors());
+
 app.use(express.json());
 
 app.use('/api/producers', producersRouter);
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://localhost:${PORT}`);
